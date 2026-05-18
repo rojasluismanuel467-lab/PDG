57107db39cfd9707949d9fac39782f44e02af38f
Luis Manuel Rojas <rojasluismanuel467@gmail.com>
2026-05-17 19:12:39 -0500

    sync: update from Bitbucket 2026-05-17 19:12

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,7 +9,7 @@ from app.core.config import settings
 from app.models import Base  # noqa: F401
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL or settings.LOCAL_DATABASE_URL))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
